184e0363a4645d69cada2fc096d49d96bf40f238
RanggaAgera <akuhpintar123@gmail.com>
2023-10-11 11:14:22 +0700

    First

---
+++ tugas2.js
@@ -0,0 +1,26 @@
+var dataSiswa = [
+    {
+        id: 1,
+        nama: "John",
+        umur: 17,
+        kelas: "10A"
+    },
+    {
+        id: 2,
+        nama: "Alice",
+        umur: 16,
+        kelas: "11B"
+    },
+    {
+        id: 3,
+        nama: "Bob",
+        umur: 18,
+        kelas: "9C"
+    }
+];
+
+// Contoh akses data pada array
+console.log("ID: " + dataSiswa[0].id);
+console.log("Nama: " + dataSiswa[0].nama);
+console.log("Umur: " + dataSiswa[0].umur);
+console.log("Kelas: " + dataSiswa[0].kelas);
